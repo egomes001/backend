62b976b3cb0babd4539fe636587ff2992a54b77f
Enzo Gomes <enzo-gomes@hotmail.fr>
2024-06-08 15:44:53 +0200

    part 4

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const auth = require('../middleware/auth')
 const router = express.Router();
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer');
 const stuffCtrl = require('../controllers/stuff');
 
 
 router.get('/', auth, stuffCtrl.getAllThings);
-router.post('/', auth, stuffCtrl.createThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
 
