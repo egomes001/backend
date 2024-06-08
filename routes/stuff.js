4b8a8ff9b7ad786ecf24f41ef029ebeedd4dd0ee
Enzo Gomes <enzo-gomes@hotmail.fr>
2024-06-08 14:27:26 +0200

    part 3

---
+++ routes/stuff.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const auth = require('../middleware/auth')
+const router = express.Router();
+const stuffCtrl = require('../controllers/stuff');
+
+
+router.get('/', auth, stuffCtrl.getAllThings);
+router.post('/', auth, stuffCtrl.createThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+
+module.exports = router;
